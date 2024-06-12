6d753e9dc9df8b4854c5391ff541e668957feb89
Tudzy <narongsak5az@gmail.com>
2024-06-12 21:28:38 +0700

    create customer

---
+++ backend/src/models/customer.model.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+const customerSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        enum: title,
+        require: true,
+    },
+    firstName: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    lastName: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    tel: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    address: {
+        type: String,
+        require: true,
+        unique: true
+    }
+})

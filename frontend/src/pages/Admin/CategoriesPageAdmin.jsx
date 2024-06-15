d77f3ab0e29c57f580832980c4ec87162fe10709
Tudzy <narongsak5az@gmail.com>
2024-06-16 00:14:36 +0700

    add title page

--- frontend/src/pages/Admin/CategoriesPageAdmin.jsx
+++ frontend/src/pages/Admin/CategoriesPageAdmin.jsx
@@ -5,6 +5,21 @@ import CreateCategory from '../../components/view/admin/formCreate/CreateCategor
 const CategoriesPageAdmin = () => {
   return (
     <div className=' m-3'>
+      <div className="content-header">
+        <div className="container-fluid">
+          <div className="row mb-2">
+            <div className="col-sm-6">
+              <h1 className="m-0">ประเภทสินค้า</h1>
+            </div>{/* /.col */}
+            <div className="col-sm-6">
+              <ol className="breadcrumb float-sm-right">
+                <li className="breadcrumb-item"><a href="#">Home</a></li>
+                <li className="breadcrumb-item active">categories</li>
+              </ol>
+            </div>{/* /.col */}
+          </div>{/* /.row */}
+        </div>{/* /.container-fluid */}
+      </div>
       <CreateCategory />
       <Categories />
     </div>
--- frontend/src/pages/Admin/formEdit/EditCategoryPageAdmin.jsx
+++ frontend/src/pages/Admin/formEdit/EditCategoryPageAdmin.jsx
@@ -5,8 +5,23 @@ import Categories from '../../../components/view/admin/Categories'
 const EditCategoryPageAdmin = () => {
   return (
     <div className='m-3'>
-        <EditCategory/>
-        <Categories/>
+      <div className="content-header">
+        <div className="container-fluid">
+          <div className="row mb-2">
+            <div className="col-sm-6">
+              <h1 className="m-0">แก้ไขข้อมูล ประเภทสินค้า</h1>
+            </div>{/* /.col */}
+            <div className="col-sm-6">
+              <ol className="breadcrumb float-sm-right">
+                <li className="breadcrumb-item"><a href="#">Home</a></li>
+                <li className="breadcrumb-item active">edit</li>
+              </ol>
+            </div>{/* /.col */}
+          </div>{/* /.row */}
+        </div>{/* /.container-fluid */}
+      </div>
+      <EditCategory />
+      <Categories />
     </div>
   )
 }
